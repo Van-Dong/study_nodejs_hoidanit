b46acb6798ad7056a919e5685c7da0f37d847642
Nguyen Van Dong <DongNV@vietis.vn>
2024-11-27 15:42:51 +0700

    Add Database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const path = require('path')
 require('dotenv').config()
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./route/web')
+const connection = require('./config/database')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,6 +11,14 @@ const hostname = process.env.HOSTNAME
 
 // Config template engine 
 configViewEngine(app)
+
+// A simple SELECT query
+connection.query(
+  'SELECT * FROM Users u',
+  function (err, results, fields) {
+    console.log('>>> Result: ', results); // results contains rows returned by server 
+  }
+);
 
 // route
 app.use('/test', webRoutes)
